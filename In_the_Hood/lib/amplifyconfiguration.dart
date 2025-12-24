93affa17a88ce627c2741f9e1475240e0f3cdcc5
raymondwhite47 <59567964+raymondwhite47@users.noreply.github.com>
2025-12-24 01:24:51 -0800

    Fix app entrypoint and add Amplify config

---
+++ In_the_Hood/lib/amplifyconfiguration.dart
@@ -0,0 +1,15 @@
+const amplifyconfig = r'''
+{
+  "UserAgent": "aws-amplify-cli/2.0",
+  "Version": "1.0",
+  "api": {
+    "plugins": {}
+  },
+  "auth": {
+    "plugins": {}
+  },
+  "storage": {
+    "plugins": {}
+  }
+}
+''';
